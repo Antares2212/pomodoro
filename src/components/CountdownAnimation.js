3fb810a95c781b833f1b89599df13ce261ac9f32
alex_shatailov <Alakey812@yandex.ru>
2023-04-06 12:46:01 +0300

    init commit

---
+++ src/components/CountdownAnimation.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import { SettingsContext } from '../context/SettingsContext'
+
+const CountdownAnimation = ({myKey = 1, timer = 20, animate = true, children}) => {
+  const {stopAimate} = useContext(SettingsContext)
+  
+  return (
+    <CountdownCircleTimer
+      key={myKey}
+      isPlaying={animate}
+      duration={timer * 60}
+      colors={[
+        ['#FE6F6B', 0.33],
+        ['#FE6F6B', 0.33],
+        ['#FE6F6B', 0.33],
+      ]}
+      strokeWidth={6}
+      size={220}
+      trailColor="#151932"
+      onComplete={ () => {
+        stopAimate()
+      }}
+    >
+        {children}
+    </CountdownCircleTimer>
+  )
+}
+
+export default CountdownAnimation
